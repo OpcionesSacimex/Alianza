947167db86d870a22e32a6778a215c5cafb0927c
azervantes <102638235+azervantes@users.noreply.github.com>
2024-03-15 11:09:29 -0600

    cambio de color

--- frontend/src/layouts/Home/HomeLayout.jsx
+++ frontend/src/layouts/Home/HomeLayout.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
-import {Menubar} from "primereact/menubar"
 import { useOutlet } from 'react-router-dom';
 import {PanelGrid} from "./../../globalsComponents/panels/PanelGrid"
 import {PanelCenter} from "./../../globalsComponents/panels/PanelCenter"
+import { Toolbar } from 'primereact/toolbar';
+        
 const HomeLayout = () => {
     const outlet = useOutlet();
     const logo= "https://convenio.opcionessacimex.com/img/logo.png";
     const start = ()=>{
         return (
             <>
-            <div style={{width:"50rem"}}>
+            <div style={{width:"13rem"}}>
             <PanelGrid>
-                    <div className="col-12 md:col-6">
+                <div className="col-12 md:col-6">
                     <a href="https://convenio.opcionessacimex.com">
                         <img alt="logo" src={logo} heigth="80" width="250"></img>
                     </a>
                 </div>
-                <div className="col-12 md:col-6">
-                    <PanelCenter>
-                        <div className="mt-3">
-                    <label className="text-xl mt-4 text-green-800">Tu crédito de confianza.</label>
-                    </div>
-                    </PanelCenter>    
-                </div>        
             </PanelGrid>
             </div>
             </>
         )
-        
+    }
+    const end =() => {
+        return (
+            <>
+                <div className="col-12">
+                    <PanelCenter>
+                    <div className="">
+                    <label className="text-xl mt-4 text-white">Tu crédito de confianza.</label>
+                    </div>
+                    </PanelCenter>    
+                </div>  
+            </>      
+        )
     }
     return (
-        <div>
-            <Menubar start={start}/>
+        <div> 
+            <Toolbar className='bg-green-800' start={start} end={end}/>
             {outlet}
         </div>
     )
