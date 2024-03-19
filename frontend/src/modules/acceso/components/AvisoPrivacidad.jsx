46c4f52b571746617be9d97738810f29f31ccd1d
PineloLopezJoseEduardo <17161206@itoaxaca.edu.mx>
2024-03-19 09:50:23 -0600

    imegess

--- frontend/src/modules/acceso/components/AvisoPrivacidad.jsx
+++ frontend/src/modules/acceso/components/AvisoPrivacidad.jsx
@@ -1,7 +1,7 @@
 import { PanelCenter } from "../../../globalsComponents/panels/PanelCenter"
 import {URLStorage} from "../../../utils/URLBackend"
 export const AvisoPrivacidad=()=>{
-    const image = `${URLStorage}/img/vrdsacinfondo`
+    const image = `${URLStorage}/img/vrdsacinfondo.png`
     return (
         <div className="font-bold" style={{
             backgroundImage: `url(${image})`, backgroundRepeat:"repeat", backgroundSize:104,
