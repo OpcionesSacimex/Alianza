bf3c0a989557f5c00ea85af90af6a7a7d2356f1e
PineloLopezJoseEduardo <17161206@itoaxaca.edu.mx>
2024-03-20 16:14:42 -0600

    api

--- BackendAlianza/app/Http/Controllers/UsuarioController.php
+++ BackendAlianza/app/Http/Controllers/UsuarioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Usuario;
 
 class UsuarioController extends Controller
 {
@@ -13,9 +14,13 @@ class UsuarioController extends Controller
         ],200);
     }
     public function store(Request $request){
+        Usuario::create([
+            "correo"=>$request->correo,
+            "password"=>$request->password,
+        ]);
+
         return response()->json([
             'status'=>true,
-            'ARES'=>"NOP"
         ],200);
     }
     public function show($id){
--- BackendAlianza/routes/api.php
+++ BackendAlianza/routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 
-Route::post('/user',[UsuarioController::class,'store']);;
+Route::post('/usuario/createUser',[UsuarioController::class,'store']);;
