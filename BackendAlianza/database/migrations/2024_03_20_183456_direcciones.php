9a8a805faa6b5d0be74290748fcac88b1e386d6a
DuilioAquino10 <79931792+DuilioAquino10@users.noreply.github.com>
2024-03-20 12:57:51 -0600

    Cambios migration
    Direccion
    economico
    convenio

---
+++ BackendAlianza/database/migrations/2024_03_20_183456_direcciones.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('direcciones', function (Blueprint $table) {
+            $table->id();
+            $table->string("calle",45);
+            $table->string("no_exterior",8);
+            $table->string("no_interior",8);
+            $table->string("cp",5);
+            $table->string("colonia",45);
+            $table->string("latitud",45);
+            $table->string("longitud",45);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::drop('direcciones');
+    }
+};
