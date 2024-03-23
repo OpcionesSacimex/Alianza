342d12800919ee77dd55a6b600f9099874d31dab
DuilioAquino10 <79931792+DuilioAquino10@users.noreply.github.com>
2024-03-23 11:50:33 -0600

    Controller of person

--- BackendAlianza/app/Http/Controllers/PersonaController.php
+++ BackendAlianza/app/Http/Controllers/PersonaController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Persona;
 
 class PersonaController extends Controller
 {
-    //
+    public function create (Request $request){
+        Persona::create([
+            "nombre"=>$request->nombre,
+            "ape_pat"=>$request->ape_pat,
+            "apr_mat"=>$request->apr_mat,
+            "telefono"=>$request->telefono
+        ]);
+
+        return response()->json([
+            'status'=>true,
+            'nombre'=> 'nombre'
+        ],200);
+    }
 }
--- BackendAlianza/routes/api.php
+++ BackendAlianza/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
@@ -7,3 +8,5 @@ use App\Http\Controllers\UsuarioController;
 Route::post('/usuario/createUser',[UsuarioController::class,'create']);
 Route::middleware('auth:api')->get('/usuario/user',[UsuarioController::class,'getUser']);
 Route::post('/usuario/login',[UsuarioController::class,'login']);
+
+Route::post('/persona/createUser',[PersonaController::class,'create']);
