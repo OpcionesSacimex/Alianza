67257b2dfa22d06e0c9dc3327ca8beba4c0d32c1
DuilioAquino10 <79931792+DuilioAquino10@users.noreply.github.com>
2024-03-23 12:08:08 -0600

    Controller of direccion

--- BackendAlianza/app/Http/Controllers/DireccionController.php
+++ BackendAlianza/app/Http/Controllers/DireccionController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Direccion;
 
 class DireccionController extends Controller
 {
-    //
+    public function create (Request $request){
+        Direccion::create([
+            "calle"=>$request->calle,
+            "no_exterior"=>$request->no_exterior,
+            "no_interior"=>$request->no_interior,
+            "cp"=>$request->cp,
+            "colonia"=>$request->colonia,
+            "latitud"=>$request->latitud, 
+            "longitud"=>$request->longitud
+        ]);
+
+        return response()->json([
+            'status'=>true,
+            'nombre'=> 'nombre'
+        ],200);
+    }
 }
--- BackendAlianza/routes/api.php
+++ BackendAlianza/routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Http\Controllers\DireccionController;
 use App\Http\Controllers\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
+use Whoops\Run;
 
 Route::post('/usuario/createUser',[UsuarioController::class,'create']);
 Route::middleware('auth:api')->get('/usuario/user',[UsuarioController::class,'getUser']);
 Route::post('/usuario/login',[UsuarioController::class,'login']);
 
 Route::post('/persona/createUser',[PersonaController::class,'create']);
+Route::post('/direccion/createDireccion',[DireccionController::class,'create']);
