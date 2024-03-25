a23376522e2e571bbbf1fb61f1a7b764a4b0100d
PineloLopezJoseEduardo <17161206@itoaxaca.edu.mx>
2024-03-25 11:13:03 -0600

    tokenes

--- BackendAlianza/routes/api.php
+++ BackendAlianza/routes/api.php
@@ -8,9 +8,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 use Whoops\Run;
 
-Route::post('/usuario/createUser',[UsuarioController::class,'create']);
-Route::middleware('auth:api')->get('/usuario/user',[UsuarioController::class,'getUser']);
+//acceso y usuario
+/*Route::prefix([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+])->group(function () {
+    //Route::post('/usuario/login', [suarioController::class,'login']);
+    Route::post('/usuario/logout', [UsuarioController::class,'logout']);
+    Route::post('/usuario/refresh',[UsuarioController::class,'refresh']);
+    Route::post('/usuario/user', [UsuarioController::class,'getUser']);
+});*/
+
+
+//Route::post('/usuario/createUser',[UsuarioController::class,'create']);
+//Route::middleware('auth:api')->get('/usuario/user',[UsuarioController::class,'getUser']);
 Route::post('/usuario/login',[UsuarioController::class,'login']);
+Route::post('/usuario/refresh',[UsuarioController::class,'refresh']);
+Route::middleware('auth:api')->get('/usuario/user', [UsuarioController::class,'getUser']);
+Route::post('/usuario/logout', [UsuarioController::class,'logout']);
+
+
+
 
 Route::post('/persona/createUser',[PersonaController::class,'create']);
 Route::post('/direccion/createDireccion',[DireccionController::class,'create']);
