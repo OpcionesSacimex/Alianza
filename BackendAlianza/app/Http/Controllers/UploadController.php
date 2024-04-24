4c26c66c405f8b07c7b9d35899f2ccbc102cc98b
DuilioAquino10 <79931792+DuilioAquino10@users.noreply.github.com>
2024-04-24 17:35:32 -0600

    Actalizacion UploadController

--- BackendAlianza/app/Http/Controllers/UploadController.php
+++ BackendAlianza/app/Http/Controllers/UploadController.php
@@ -10,37 +10,57 @@ class UploadController extends Controller
 
     public function uploadDocument(Request $request) {
         // Verificar si se han enviado los archivos
-        if ($request->hasFile('ine') && $request->hasFile('croquis') && $request->hasFile('foto') && $request->hasFile('comprobante_dom') && $request->hasFile('comprobante_ing')) {
+        if ($request->hasFile('ine') && $request->hasFile('fotografía') && $request->hasFile('acta_nac') && $request->hasFile('curp') && $request->hasFile('rfc') && $request->hasFile('comprobante_dom') && $request->hasFile('croquis') && $request->hasFile('comprobante_ing') && $request->hasFile('comprobante_ing2') && $request->hasFile('comprobante_ing3')) {
             try {
                 // Validar los archivos
                 $request->validate([
                     'ine' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
-                    'croquis' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
-                    'foto' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
+                    'fotografía' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
+                    'acta_nac' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
+                    'curp' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
+                    'rfc' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
                     'comprobante_dom' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
+                    'croquis' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
                     'comprobante_ing' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
+                    'comprobante_ing2' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
+                    'comprobante_ing3' => 'required|mimetypes:application/pdf,image/jpeg,image/png|max:2048',
                 ]);
 
                 // Obtener los archivos
                 $ine = $request->file('ine');
+                $fotografía = $request->file('fotografía');
+                $acta_nac = $request->file('acta_nac');
+                $curp = $request->file(('curp'));
+                $rfc = $request->file(('rfc'));
+                $comprobante_dom = $request->file('comprobante_dom');
                 $croquis = $request->file('croquis');
-                $foto = $request->file('foto');
-                $comprobante_dom = $request->file(('comprobante_dom'));
-                $comprobante_ing = $request->file(('comprobante_ing'));
+                $comprobante_ing = $request->file('comprobante_ing');
+                $comprobante_ing2 = $request->file(('comprobante_ing2'));
+                $comprobante_ing3 = $request->file(('comprobante_ing3'));
 
                 // Generar nombres únicos para los archivos
                 $ineNom = uniqid('ine_') . '.' . $ine->getClientOriginalExtension();
+                $fotografíaNom = uniqid('fotografía_') . '.' . $fotografía->getClientOriginalExtension();
+                $acta_nacNom = uniqid('acta_nac_') . '.' . $acta_nac->getClientOriginalExtension();
+                $curpNom = uniqid('curp_').'.'. $curp->getClientOriginalExtension();
+                $rfcNom = uniqid('rfc_').'.'. $rfc->getClientOriginalExtension();
+                $comprobante_domNom = uniqid('comprobante_dom_') . '.' . $comprobante_dom->getClientOriginalExtension();
                 $croquisNom = uniqid('croquis_') . '.' . $croquis->getClientOriginalExtension();
-                $fotoNom = uniqid('foto_') . '.' . $foto->getClientOriginalExtension();
-                $comprobante_domNom = uniqid('comprobante_domicilio_').'.'. $comprobante_dom->getClientOriginalExtension();
-                $comprobante_ingNom = uniqid('comprobante_ingresos_').'.'. $comprobante_ing->getClientOriginalExtension();
+                $comprobante_ingNom = uniqid('comprobante_ing_') . '.' . $comprobante_ing->getClientOriginalExtension();
+                $comprobante_ing2Nom = uniqid('comprobante_ing2_').'.'. $comprobante_ing2->getClientOriginalExtension();
+                $comprobante_ing3Nom = uniqid('comprobante_ing3_').'.'. $comprobante_ing3->getClientOriginalExtension();
 
                 // Mover los archivos a la carpeta storage
                 $ine->move(storage_path('app/public'), $ineNom);
+                $fotografía->move(storage_path('app/public'), $fotografíaNom);
+                $acta_nac->move(storage_path('app/public'), $acta_nacNom);
+                $curp->move(storage_path('app/public'),$curpNom);
+                $rfc->move(storage_path('app/public'),$rfcNom);
+                $comprobante_dom->move(storage_path('app/public'), $comprobante_domNom);
                 $croquis->move(storage_path('app/public'), $croquisNom);
-                $foto->move(storage_path('app/public'), $fotoNom);
-                $comprobante_dom->move(storage_path('app/public'),$comprobante_domNom);
-                $comprobante_ing->move(storage_path('app/public'),$comprobante_ingNom);
+                $comprobante_ing->move(storage_path('app/public'), $comprobante_ingNom);
+                $comprobante_ing2->move(storage_path('app/public'),$comprobante_ing2Nom);
+                $comprobante_ing3->move(storage_path('app/public'),$comprobante_ing3Nom);
 
                 return response()->json(['status' => true, 'message' => 'Archivos subidos correctamente'], 200);
             } catch (\Exception $error) {
@@ -52,16 +72,31 @@ class UploadController extends Controller
             if (!$request->hasFile('ine')) {
                 $missingFiles[] = 'INE';
             }
+            if (!$request->hasFile('fotografía')) {
+                $missingFiles[] = 'CROQUIS';
+            }
+            if (!$request->hasFile('acta_nac')) {
+                $missingFiles[] = 'ACTA DE NACIMIENTO';
+            }
+            if (!$request->hasFile('curp')) {
+                $missingFiles[] = 'CURP';
+            }
+            if (!$request->hasFile('rfc')) {
+                $missingFiles[] = 'RFC';
+            }
+            if (!$request->hasFile('comprobante_dom')) {
+                $missingFiles[] = 'COMPROBANTE_Domicilio';
+            }
             if (!$request->hasFile('croquis')) {
                 $missingFiles[] = 'CROQUIS';
             }
-            if (!$request->hasFile('foto')) {
-                $missingFiles[] = 'Foto';
-            }
-            if (!$request->hasFile('comprobante_dom')) {
-                $missingFiles[] = 'COMPROBANTE_DOMICILIO';
-            }
             if (!$request->hasFile('comprobante_ing')) {
+                $missingFiles[] = 'COMPROBANTE_INGRESOS';
+            }
+            if (!$request->hasFile('comprobante_ing2')) {
+                $missingFiles[] = 'COMPROBANTE_INGRESOS';
+            }
+            if (!$request->hasFile('comprobante_ing3')) {
                 $missingFiles[] = 'COMPROBANTE_INGRESOS';
             }
             $errorMessage = 'Faltan los siguientes archivos: ' . implode(', ', $missingFiles);
